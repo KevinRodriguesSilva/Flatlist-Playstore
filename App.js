588ac4ed123c81b71758b4a126c5eae4d1ce0f87
KevinRodriguesSilva <kevin.ponas@gmail.com>
2022-05-03 11:35:40 -0300

    Flatlist não finalizado

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ export default function App() {
   return (
     <View style={{backgroundColor:'#D3D3D3', flex:1}}>
       <Cabecalho></Cabecalho>
+      <View>
       <Botao
         Logo='md-game-controller' texto="jogos" cor="blue"
         Logo2='apps' texto2="aplicativos" cor2="red">
@@ -16,7 +17,22 @@ export default function App() {
         Logo='md-game-controller' texto="filmes" cor="pink"
         Logo2='apps' texto2="musicas" cor2="darkgreen">
       </Botao>
+      </View> 
+      <View>
+        <FlatList
+          horizontal={true}
+          data={ Dados}
+          keyExtractor = {(item) => item.id}
+          renderItem = { ({ item }) => (
 
-     </View>
-  );
+            <Jogo
+              título = {item.nome}
+              imagem = {item.imagem}
+              valor = {item.valor}
+            />   
+          )}
+        />
+      </View>
+    </View>
+  );    
 }
---
+++ dados/jogos.js
@@ -0,0 +1,34 @@
+const JOGOS = [
+    {
+        id : "01",
+        nome: "Pokemons Masters",
+        valor: 20.00,
+        imagem: 'icon.png'
+    },
+    {
+        id: "02",
+        nome: "Subway Surfers",
+        valor : "gratuito",
+        imagem: "subway.png"
+    },
+    {
+        id: "03",
+        nome: "Subway Surfers",
+        valor : "gratuito",
+        imagem: "subway.png"
+    },
+    {
+        id: "04",
+        nome: "Subway Surfers",
+        valor : "gratuito",
+        imagem: "subway.png"
+    },
+    {
+        id: "05",
+        nome: "Subway Surfers",
+        valor : "gratuito",
+        imagem: "subway.png"
+    }
+]
+
+export default JOGOS;
